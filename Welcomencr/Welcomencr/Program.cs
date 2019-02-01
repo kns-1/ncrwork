c4a4049bc48ba2ab9ac2dfada30a5fde499ab22d
kns-1 <navyakovuru@gmail.com>
2019-02-01 10:41:46 -0800

    comment added

--- Welcomencr/Welcomencr/Program.cs
+++ Welcomencr/Welcomencr/Program.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//first time using c#
 namespace Welcomencr
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
