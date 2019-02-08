eaf4c6a273035ada9ac52489f869fc327fbbbc44
Navya <navyakovuru@gmail.com>
2019-02-08 14:47:49 +0530

    Playing with 2D Array

--- Assignment-3/sample2DArray/sample2DArray.c
+++ Assignment-3/sample2DArray/sample2DArray.c
@@ -2,15 +2,15 @@
 #include<conio.h>
 void main()
 {
-	float table[2][3] = { {1.1,1.2,1.3},{2.1,2.2,2.3} };
+	float table[2][3] = { {1.1,1.2,1.3},{2.1,2.2,2.3}};
 	//the table is a 2-D array of floating point numbers
 	printf("(table+1) = %d\n", (table + 1));
 	printf("*(table+1) = %d\n", *(table + 1));
 	printf("(*(table+1)+1) = %d\n", (*(table + 1) + 1));
 	printf("(*(table)+1) = %d\n", (*(table)+1));
-	printf("*(*(table+1)+1) = %d\n", *(*(table + 1) + 1));
-	printf("*(*(table)+1) = %d\n", *(*(table)+1));
-	printf("*(*(table+1)) = %d\n", *(*(table + 1)));
-	printf("*(*(table)+1)+1 = %d", *(*(table)+1) + 1);
+	printf("*(*(table+1)+1) = %f\n", *(*(table + 1) + 1));
+	printf("*(*(table)+1) = %f\n", *(*(table)+1));
+	printf("*(*(table+1)) = %f\n", *(*(table + 1)));
+	printf("*(*(table)+1)+1 = %f", *(*(table)+1) + 1);
 	_getch();
 }
