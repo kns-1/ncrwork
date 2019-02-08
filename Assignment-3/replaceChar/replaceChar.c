0a0798807d3b105f339cc14ab9387028b539fdec
Navya <navyakovuru@gmail.com>
2019-02-08 15:24:01 +0530

    Replacing spaces with '-'

---
+++ Assignment-3/replaceChar/replaceChar.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int replace(char *s)
+{
+	int n=0, i;
+	for (i = 0; i < strlen(s); i++)
+	{
+		if (s[i] == ' ')
+		{
+			s[i]='-';
+			n++;
+		}
+	}
+	printf("String is: %s\n", s);
+	return n;
+}
+void main()
+{
+//	char *cat = "The cat sat"; //gives error
+	char *cat, s[100]= "The cat sat";
+	int n;
+	cat = (char *)malloc(sizeof(char *));
+	strcpy(cat,s);
+	n = replace(cat);
+	printf("Number of replacements = %d", n);
+	_getch();
+}
