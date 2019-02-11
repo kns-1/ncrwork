e07f69c3d56a8a71b8c2b31e8d04200e6c924933
Navya <navyakovuru@gmail.com>
2019-02-11 17:58:21 +0530

    Assignment -3 changes

--- Assignment-3/SamplePointers/samplePointers.c
+++ Assignment-3/SamplePointers/samplePointers.c
@@ -5,9 +5,9 @@ void main()
 	int x[8] = {10,20,30,40,50,60,70,80};
 	//Here, x is an array of 8 integers
 	printf("x = %d\n", x); //x gives base address
-	printf("(x+2)= %d\n", (x + 2)); //third element address
-	printf("*x = %d\n", *x); //value in x[0]
-	printf("(*x + 2) = %d\n", (*x + 2)); //value in x[0]+2
-	printf("*(x+2) = %d", *(x + 2)); //third element
+	printf("(x+2)= %d\n", (x + 2)); //gives third element address
+	printf("*x = %d\n", *x); //gives value in x[0]
+	printf("(*x + 2) = %d\n", (*x + 2)); //gives value in x[0]+2
+	printf("*(x+2) = %d", *(x + 2)); //gives the third element
 	_getch();
 }
