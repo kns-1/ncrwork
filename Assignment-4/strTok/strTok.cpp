8e734088957daa48a54c91e688d08dccfa42dfcf
Navya <navyakovuru@gmail.com>
2019-02-12 22:50:59 +0530

    Implementing "strtok" function

---
+++ Assignment-4/strTok/strTok.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream>
+void stringToken(char *s, char t)
+{
+	int i;
+	for (i = 0; i < strlen(s); i++)
+	{
+		if (s[i] == t)
+		{
+			printf("\n");
+			continue;
+		}
+		printf("%c", s[i]);
+	}
+}
+void main()
+{
+	char *s;
+	char t;
+	s = (char *)malloc(100 * sizeof(char));
+	printf("enter token: ");
+	scanf_s("%c", &t);
+	printf("enter string with token: ");
+	scanf_s(" %[^\n]s", s, 100);
+	stringToken(s, t);
+	free(s);
+	_getch();
+}
