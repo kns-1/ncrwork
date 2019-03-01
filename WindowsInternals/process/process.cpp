9fa6f3042aae03d99d9054802c7a9058e645d123
Navya <navyakovuru@gmail.com>
2019-03-01 17:20:43 +0530

    Windows Programming

--- WindowsInternals/process/process.cpp
+++ WindowsInternals/process/process.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 void main(int argc, LPWSTR *argv)
 {
-	STARTUPINFO startupInfo;
-	PROCESS_INFORMATION processInfo;
+	STARTUPINFO startupInfo, startupInfo1;
+	PROCESS_INFORMATION processInfo, processInfo1;
 	//the process info handle must be closed at the end
 
 	ZeroMemory(&startupInfo, sizeof(startupInfo));
 	ZeroMemory(&processInfo, sizeof(processInfo));
+	ZeroMemory(&startupInfo1, sizeof(startupInfo1));
+	ZeroMemory(&processInfo1, sizeof(processInfo1));
 //CHILD-1
 	int returnValueProcess = CreateProcess(TEXT("D:/WinObj.exe"), NULL, NULL, NULL, FALSE, 0, NULL, NULL,&startupInfo ,&processInfo);
 	if (0 == returnValueProcess)
@@ -22,8 +24,14 @@ void main(int argc, LPWSTR *argv)
 
 	cout << "Startup Info size is: " << sizeof(startupInfo)<< endl;
 	cout << "Process Info size is : " << sizeof(processInfo) << endl;
-	cout << "Process ID is : " << sizeof(processInfo.dwProcessId) << endl;
-	cout << "Process Tread ID is : " << sizeof(processInfo.dwThreadId) << endl;
+	cout << "Process ID is : " << processInfo.dwProcessId<< endl;
+	cout << "Process Tread ID is : " << processInfo.dwThreadId << endl;
+	
+	cout << "Current Process ID: " << GetCurrentProcessId() << endl;
+	cout << "Current Thread ID: " << GetCurrentThreadId() << endl;
+	cout << "Get Process ID: " << GetProcessId << endl;
+	cout << "Get Thread ID: " << GetThreadId << endl;
+	cout << "Process ID of Thread: " << GetProcessIdOfThread << endl;
 
 	WaitForSingleObject(processInfo.hProcess, INFINITE); // waits for the first process/application to be closed, only then the next process is executed.
 	CloseHandle(processInfo.hProcess);
@@ -31,7 +39,7 @@ void main(int argc, LPWSTR *argv)
 	
 
 //CHILD-2
-	int returnValueProcess1 = CreateProcess(TEXT("D:/Calculator.exe"), argv[1], NULL, NULL, FALSE, 0, NULL, NULL, &startupInfo, &processInfo);
+	int returnValueProcess1 = CreateProcess(TEXT("D:/Calculator.exe"), argv[1], NULL, NULL, FALSE, 0, NULL, NULL, &startupInfo1, &processInfo1);
 	if (0 == returnValueProcess1)
 	{
 		cout << "CreateProcess() failed, Error code is: " << GetLastError() << endl;
@@ -40,12 +48,19 @@ void main(int argc, LPWSTR *argv)
 	}
 	cout << "CHILD-2 \nReturn value of CreateProcess() is: " << returnValueProcess1 << endl;
 
-	cout << "Startup Info size is: " << sizeof(startupInfo) << endl;
-	cout << "Process Info is : " << sizeof(processInfo) << endl;
-	cout << "Process ID is : " << sizeof(processInfo.dwProcessId) << endl;
-	cout << "Process Tread ID is : " << sizeof(processInfo.dwThreadId) << endl;
-	WaitForSingleObject(processInfo.hProcess, INFINITE);
-	CloseHandle(processInfo.hProcess);
-	CloseHandle(processInfo.hThread);
+	cout << "Startup Info size is: " << sizeof(startupInfo1) << endl;
+	cout << "Process Info is : " << sizeof(processInfo1) << endl;
+	cout << "Process ID is : " << processInfo1.dwProcessId << endl;
+	cout << "Process Tread ID is : " << processInfo1.dwThreadId << endl;
+
+	cout << "Current Process ID: " << GetCurrentProcessId() << endl;
+	cout << "Current Thread ID: " << GetCurrentThreadId() << endl;
+	cout << "Get Process ID: " << GetProcessId << endl;
+	cout << "Get Thread ID: " << GetThreadId << endl;
+	cout << "Process ID of Thread: " << GetProcessIdOfThread << endl;
+
+	WaitForSingleObject(processInfo1.hProcess, INFINITE);
+	CloseHandle(processInfo1.hProcess);
+	CloseHandle(processInfo1.hThread);
 	system("pause");
 }
